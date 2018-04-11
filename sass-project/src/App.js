f6dae0be8b336d6ce534b224754068637b37a783
Wombeez <jdpeck90@gmail.com>
2018-04-11 18:14:15 -0400

    Add finished Sass Project

--- sass-project/src/App.js
+++ sass-project/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
-import './App.scss';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1 className="title">Hello</h1>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
