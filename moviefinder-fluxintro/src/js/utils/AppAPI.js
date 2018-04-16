033000dac51d419f7eca42651e8e6c55d1088ad5
wombeez <jpeck@intuitsolutions.net>
2018-04-16 09:59:19 -0400

    get rid of failed attempts of movie finder

--- moviefinder-fluxintro/src/js/utils/AppAPI.js
+++
@@ -1,8 +0,0 @@
-var AppActions = require('../actions/AppActions');
-
-
-module.exports = {
-    searchMovies: function(movie){
-
-    }
-}
--- moviefindertwo/src/js/main.js
+++
@@ -1,6 +0,0 @@
-var App = reqiure('./components/App');
-var React = reqiure('react');
-var ReactDom = reqiure('react-dom');
-var AppAPI = reqiure('./utils/AppApi.js');
-
-ReactDOM.render(<App />, document.getElementById('app'));
